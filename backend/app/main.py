7d163081da1d0b1e2dfcab1954f9b591117d7220
pradeep04072006-commits <pradeep04072006@gmail.com>
2026-07-05 08:15:32 +0530

    Changes

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,7 @@ app = FastAPI(
 )
 
 # CORS configuration
-allowed_origins = [settings.FRONTEND_URL, "https://skill-gap-analysis-gs.vercel.app"]
+allowed_origins = [settings.FRONTEND_URL, "http://localhost:5173/"]
 
 app.add_middleware(
     CORSMiddleware,
